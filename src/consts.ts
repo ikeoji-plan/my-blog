e54b9590820445a6cc3d4a81cd2bdeedf02bdea7
轟洋祐 <todorokiyousuke@yokke2024.local>
2026-05-12 23:08:18 +0900

    ブログタイトルを変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'イケおじ計画〜40歳からの自己改造記〜';
+export const SITE_DESCRIPTION = '40歳からイケてるおじさんを目指す自己改造ブログ。英語・読書・体型管理・副業に挑戦中。';
